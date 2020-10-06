d5c4b6c9364f09b1d3e7aba4c83f2f5d535b873d
mariegross <marie.gross@gmx.de>
2020-10-06 15:43:53 +0200

    Add more buttons

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,10 +1,14 @@
 import "./button.css";
 import buttonText from "./button-text.html";
 import buttonLogo from "./button-logo.html";
-import buttonPlay from "./play-text.html";
+import buttonplay from "./play-text.html";
+import buttonaddFavorites from "./favorites.html";
+import buttonShare from "./share.html";
 
 export default { title: "Components/Button" };
 
 export const withText = () => buttonText;
 export const withLogo = () => buttonLogo;
-export const buttonplay = () => buttonPlay;
+export const Play = () => buttonplay;
+export const AddFavorites = () => buttonaddFavorites;
+export const Share = () => buttonShare;
