ec150cf1579e37d8365f0aa45ff1efefae856bda
mariegross <marie.gross@gmx.de>
2020-10-07 21:19:54 +0200

    Modify nav-bottom

--- src/components/nav/nav.stories.js
+++ src/components/nav/nav.stories.js
@@ -1,6 +1,6 @@
 import "./nav.css";
 import navbottom from "./nav.html";
 
-export default { title: "Components/Nav" };
+export default { title: "Components/BottomNavigation" };
 
-export const NavBottom = () => navbottom;
+export const HomeActive = () => navbottom;
