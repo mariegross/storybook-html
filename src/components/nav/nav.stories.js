69d09e426138cadd3db272231aeac9623673e5d3
mariegross <marie.gross@gmx.de>
2020-10-12 00:06:29 +0200

    Add different nav versions

--- src/components/nav/nav.stories.js
+++ src/components/nav/nav.stories.js
@@ -1,6 +1,10 @@
 import "./nav.css";
 import navbottom from "./nav.html";
+import navbottomsongs from "./nav-s.html";
+import navbottomsettings from "./nav-sett.html";
 
 export default { title: "Components/BottomNavigation" };
 
 export const HomeActive = () => navbottom;
+export const SongsActive = () => navbottomsongs;
+export const SettingsActive = () => navbottomsettings;
